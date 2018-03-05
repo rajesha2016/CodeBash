4a87551d2cf07b50607d7a967a6e6983d90d08d8
PROWARENESSDC\r.moharana <r.moharana@devonl.nl>
2018-03-05 19:33:32 +0530

    adding LinkedList operation file

---
+++ Driessen.Application/src/test/java/com/driessen/Application/Driessen/Application/LinkedListOperation.java
@@ -0,0 +1,23 @@
+package com.driessen.Application.Driessen.Application;
+
+import java.util.LinkedList;
+
+public class LinkedListOperation {
+	
+	//How to delete all elements from my LinkedList?
+	
+	public static void main(String args[]) {
+		LinkedList<String> ll=new LinkedList<String>();
+		
+		ll.add("rajesha");
+		ll.add("ram");
+		ll.add("shyam");
+		
+		System.out.println("actual linked list: "+ll);
+		
+		ll.clear();
+		
+		System.out.println("after deletion: "+ll);
+	}
+
+}
